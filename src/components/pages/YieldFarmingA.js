540974507b68d34528dab8edbc1b341ce97a1fc8
Isha Gupta <ig044962@broadcom.net>
2024-03-10 18:48:34 +0530

    Yield Farming

--- src/components/pages/YieldFarmingA.js
+++ src/components/pages/YieldFarmingA.js
@@ -1,5 +1,53 @@
-import React from 'react';
+import { Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import MenuPanel from '../common/MenuPanel';
 
 export default function YieldFarmingA() {
-	return <div>YieldFarmingA</div>;
+	const [overViewMenuItems, setOverViewMenuItems] = useState([]);
+	const [balancedDetails, setBalancedDetails] = useState({
+		lpdx: 0,
+		apr: 0,
+		stakedSince: 0,
+		pdxRewards: 0,
+		ethRewards: 0,
+	});
+
+	useEffect(() => {
+		// Set everything for initial load
+		setOverViewMenuItems(['test']);
+		setBalancedDetails({
+			lpdx: 12,
+			apr: 15762,
+			stakedSince: 1723,
+			pdxRewards: 3213,
+			ethRewards: 333,
+		});
+	}, []);
+
+	const handleOverviewMenuSelection = (option) => {};
+	return (
+		<Grid container spacing={1} className=''>
+			<Grid item md={8.5} className='p-0'></Grid>
+			<Grid item md={3.5} className='p-0'>
+				<div className='container-card'>
+					<div className='grey-card'>
+						<div className='app-flex justify-content-space-between white-bottom-border card-padding'>
+							<Typography variant='h4'>
+								Overview
+							</Typography>
+							<MenuPanel
+								menuItems={overViewMenuItems}
+								onSelection={
+									handleOverviewMenuSelection
+								}
+							/>
+						</div>
+						<div className='card-padding'>
+							<div className='dark-grey-card'></div>
+						</div>
+					</div>
+				</div>
+			</Grid>
+		</Grid>
+	);
 }
