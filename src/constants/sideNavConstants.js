effb58b2a660252e3a5803e582d0f9778c3a89d7
Isha Gupta <ig044962@broadcom.net>
2024-03-10 18:04:45 +0530

    Header changes

--- src/constants/sideNavConstants.js
+++ src/constants/sideNavConstants.js
@@ -7,39 +7,53 @@ import YieldFarming from '../components/icons/YieldFarming';
 
 export const DRAWER_WIDTH = 100;
 
+export const COMMON_HEADER_NAV_PATHS = [
+	'/yield-farming-a',
+	'/yield-farming-b',
+	'/onboarding',
+];
+
+export const SHOW_ICONS_NAV_PATHS = ['/yield-farming-b'];
+
 export const MENU_ITEMS = [
 	{
 		item: 'a',
+		title: 'Dashboard',
 		icon: <Home />,
 		activeIcon: <Home color={'active'} />,
 		navPath: '/',
 	},
 	{
 		item: 'b',
+		title: 'Yield Farming',
 		icon: <YieldFarming />,
 		activeIcon: <YieldFarming color={'active'} />,
 		navPath: '/yield-farming-a',
 	},
 	{
 		item: 'c',
+		title: 'Yield Farming',
 		icon: <Charts />,
 		activeIcon: <Charts color={'active'} />,
 		navPath: '/yield-farming-b',
 	},
 	{
 		item: 'd',
+		title: 'On-boarding',
 		icon: <User />,
 		activeIcon: <User color={'active'} />,
 		navPath: '/onboarding',
 	},
 	{
 		item: 'e',
+		title: 'Dashboard',
 		icon: <Activity />,
 		activeIcon: <Activity color={'active'} />,
 		navPath: '/',
 	},
 	{
 		item: 'f',
+		title: 'Dashboard',
 		icon: <Folder />,
 		activeIcon: <Folder color={'active'} />,
 		navPath: '/',
