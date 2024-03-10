78db4829aa96224ee786e3db6155dd661bde1d3d
tarun18k <tarungupta1644@gmail.com>
2024-03-10 17:50:51 +0530

    feat: button syles

--- src/components/common/AppButton.js
+++ src/components/common/AppButton.js
@@ -5,6 +5,31 @@ import * as React from 'react';
 export default function AppButton(props) {
 	const { text, variant, endIcon, clickHandler, borderRadius, minWidth } =
 		props;
+        let bgColor,color,hoverBGColor,activeBGColor;
+        switch(variant){
+            case 'outlined':
+                bgColor='#161717';
+                color='#ffffff';
+                hoverBGColor='rgb(146, 146, 146, 0.3)';
+                activeBGColor='rgb(146, 146, 146, 0.3)';
+				break;
+
+            case 'primary':
+                bgColor='#ffffff';
+                color='#000000';
+                hoverBGColor='rgb(220 213 213)';
+                activeBGColor='rgb(220 213 213)';
+				break;
+
+            default:
+                bgColor='#161717';
+                color='#ffffff';
+                hoverBGColor='rgb(217, 217, 217,0.3)'
+                activeBGColor='rgb(217, 217, 217,0.3)'
+				break;
+
+        }
+
 
 	const BootstrapButton = styled(Button)({
 		boxShadow: 'none',
@@ -13,24 +38,22 @@ export default function AppButton(props) {
 		padding: '6px 12px',
 		border: '1px solid',
 		lineHeight: 1.5,
-		backgroundColor: '#161717',
-		borderColor: variant === 'outlined' ? '#ffffff' : '#161717',
+		backgroundColor: bgColor,
+		borderColor: variant === 'outlined' ? '#ffffff' : '',
 		borderRadius: borderRadius || 4,
 		minWidth: minWidth || '25.75rem',
 		minHeight: '2.75rem',
-		color: '#ffffff',
+		color:color,
 		fontFamily: ['Courier Prime'].join(','),
 		'&:hover': {
-			backgroundColor: 'rgb(146, 146, 146, 0.3)',
-			borderColor: 'rgb(146, 146, 146, 0.3)',
+			backgroundColor: hoverBGColor,
 			borderColor: variant === 'outlined' ? '#ffffff' : '#161717',
 			boxShadow: 'none',
 		},
 		'&:active': {
 			boxShadow: 'none',
-			backgroundColor: 'rgb(146, 146, 146, 0.3)',
+			backgroundColor: activeBGColor,
 			borderColor: variant === 'outlined' ? '#ffffff' : '#161717',
-			borderColor: 'rgb(146, 146, 146, 0.3)',
 		},
 		'&:focus': {
 			boxShadow: '0 0 0 0.2rem rgba(146, 146, 146, .5)',
